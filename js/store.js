5d2901814907e622bb62ed100147d8126f843a88
Sanjay Meher <digitalpgmb@gmail.com>
2026-01-14 23:23:59 +0530

    Update store.js

--- js/store.js
+++ js/store.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     PRODUCTS_JSON_PATH: 'data/products.json',
     GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyKUH2gF2XIvskcVGS8Qa4QU_zsVsIpyj7H30BoCyFXcbi56J2mFahDiCJ04MelsHVU4Q/exec'
-    COUPON_SCRIPT_URL: 'YOUR_NEW_COUPON_SCRIPT_URL_HERE'
+    COUPON_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzXOMtif8V3GAXKMGq4q11n-m9TMmpdlnnBg1Sjad7l5K9i4ylv3PcgS2hhqrYKnRh5/exec'
 };
 let appliedCoupon = null;
 let discountedAmount = null;
